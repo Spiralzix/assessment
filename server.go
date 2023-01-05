3dd8a91144715de2dc95ade3ffb441ef154e6be1
Pichapon <p.naksinn@gmail.com>
2023-01-06 00:48:32 +0700

    Add DB connection in server.go

--- server.go
+++ server.go
@@ -1,11 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"database/sql"
+	"log"
+
+	"github.com/Spiralzix/assessment/config"
 )
 
 func main() {
-	fmt.Println("Please use server.go for main file")
-	fmt.Println("start at port:", os.Getenv("PORT"))
+	/// Read config file
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatalf("Config error: %s", err)
+	}
+
+	/// Connect to DB and create table named expenses
+	db, err := sql.Open("postgres", cfg.PostgreSQL.Url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	createTb := `CREATE TABLE IF NOT EXISTS expenses (id SERIAL PRIMARY KEY, Title TEXT, amount INT, note TEXT, tags TEXT[]);`
+	_, err = db.Exec(createTb)
+
+	if err != nil {
+		log.Fatal("can't create table", err)
+	}
 }
