df68f4a269da8f762dd106f4c351ac57dc47339a
Pichapon <p.naksinn@gmail.com>
2023-01-07 19:07:55 +0700

    Add graceful shutdown in server.go

--- server.go
+++ server.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/Spiralzix/assessment/config"
 	"github.com/Spiralzix/assessment/handler"
@@ -41,5 +47,19 @@ func main() {
 	e.GET("/expenses", h.QueryAllExpenseHandler)
 	e.PUT("/expenses/:id", h.UpdateExpenseHandler)
 
-	log.Fatal(e.Start(cfg.Port))
+	go func() {
+		if err := e.Start(cfg.Port); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt)
+	<-shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+	fmt.Println("\nShutdown process, Done")
 }
